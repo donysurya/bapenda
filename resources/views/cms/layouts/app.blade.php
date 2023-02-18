0f469c198cfc840ee1b81e2ce8f0830dd0796166
Dony Surya Manggala <donymanggala@gmail.com>
2023-02-18 20:35:45 +0700

    GTM

--- resources/views/cms/layouts/app.blade.php
+++ resources/views/cms/layouts/app.blade.php
@@ -23,13 +23,15 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
         <script src="https://kit.fontawesome.com/19b42cd13d.js" crossorigin="anonymous"></script>
 
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-THPH67Q');</script>
-        <!-- End Google Tag Manager -->
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-THPH67Q"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'GTM-THPH67Q');
+        </script>
         
         <!-- Favicon -->
         <link rel="icon" type="image/png" href="{{ asset('/img/logo_katingan.png') }}"/>
