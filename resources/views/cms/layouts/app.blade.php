5fa758ec779d98191a41921014a506110d37a4bc
Dony Surya Manggala <donymanggala@gmail.com>
2023-02-18 20:23:54 +0700

    GTM

--- resources/views/cms/layouts/app.blade.php
+++ resources/views/cms/layouts/app.blade.php
@@ -22,6 +22,14 @@
         <!-- Icon -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
         <script src="https://kit.fontawesome.com/19b42cd13d.js" crossorigin="anonymous"></script>
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-THPH67Q');</script>
+        <!-- End Google Tag Manager -->
         
         <!-- Favicon -->
         <link rel="icon" type="image/png" href="{{ asset('/img/logo_katingan.png') }}"/>
--- resources/views/cms/login/index.blade.php
+++ resources/views/cms/login/index.blade.php
@@ -20,6 +20,14 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-THPH67Q');</script>
+        <!-- End Google Tag Manager -->
+
     <title>Administrator Bapenda | Pajak Online | Kabupaten Katingan - Kalimantan Tengah</title>
 </head>
 <body class="bg-danger">
