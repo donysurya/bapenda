e2150ea35cead20e430e7b239964ef83ffc63bf0
Dony Surya Manggala <donymanggala@gmail.com>
2023-04-09 21:22:11 +0700

    Update Bapenda V2 bug 1

--- resources/views/landingpage/main.blade.php
+++ resources/views/landingpage/main.blade.php
@@ -202,7 +202,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script src="{{ asset('js/main.js') }}"></script>
+        <script src="{{ asset('js/main.js?v2') }}"></script>
         
         <script>
             // Hide Navbar on scroll down
