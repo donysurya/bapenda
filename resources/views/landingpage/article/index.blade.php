fb07cc49bdabfb93ba9f7fe4d4a85a40b0408a2f
Dony Surya Manggala <donymanggala@gmail.com>
2023-06-05 19:26:39 +0700

    Update

--- resources/views/landingpage/article/index.blade.php
+++ resources/views/landingpage/article/index.blade.php
@@ -18,7 +18,7 @@
 @section('content')
     <div class="container-fluid mx-0 px-0">
         <div class="row gy-3 mt-2 px-3">
-            <div class="col-lg-12 p-4 pt-0">
+            <div class="col-lg-12 py-4 px-2 pt-0">
                 <div class="card shadow">
                     <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
                         <ol class="breadcrumb mb-1 py-2 px-4">
@@ -30,12 +30,12 @@
             </div>
         </div>
         <div class="row gy-3 px-3">
-            <div class="col-lg-12 p-4 pt-0">
+            <div class="col-lg-12 py-4 px-2 pt-0">
                 <div class="card h-100">
                     <div class="card-header">
                         <h5 class="text-dark fw-bold my-2"><i class="bi bi-newspaper me-2"></i>Berita Bapenda Katingan</h5>
                     </div>
-                    <div class="card-body p-4 pb-0">
+                    <div class="card-body py-4 px-2 pb-0">
                         <h3 class="fw-bold text-uppercase text-center">Berita Bapenda Kabupaten Katingan</h3>
                         <hr class="bg-secondary">
 
@@ -43,7 +43,7 @@
                             <div class="col-md-12">
                                 <div class="row mb-2">
                                     @forelse($data as $index => $item)
-                                        <div class="col-md-6">
+                                        <div class="col-md-6 p-2">
                                             <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow h-md-250 position-relative">
                                                 <div class="col p-4 d-flex flex-column position-static">
                                                     <strong class="d-inline-block mb-2 text-primary">
